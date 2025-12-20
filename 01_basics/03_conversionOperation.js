402f83af46b8af776a9d124b1581661a2fe05310
vikasyadav104 <vikasyadavskb247@gmail.com>
2025-12-20 16:38:32 +0000

    coversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score="33"
+let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+//most important thing is they all give number as its type
+//"33"=> 33
+//"33aqbc"=>NaN
+//"true"=> 1 alse "false"=>0
+//null=> 0
+//undefined=>NaN
+
+let isLoggedIn="33abc"
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+//important thing is type =booolean
+//"33"=> true
+//"33aqbc"=>true
+//null=> false
+//undefined=>false
+
+let finalScore
+let stringconverter=String(finalScore)
+console.log(typeof stringconverter)
+console.log(stringconverter)
+
+//important thing is type =string
+//33=> "33"
+//33aqbc=>"33abs"
+//null=> null
+//undefined=>undefined
