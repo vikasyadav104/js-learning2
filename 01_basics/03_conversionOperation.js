c300d60d90a0397880a0dd6f9e8ef08a35dcbaa6
vikasyadav104 <vikasyadavskb247@gmail.com>
2025-12-21 04:50:40 +0000

    changeInConversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -32,3 +32,25 @@ console.log(stringconverter)
 //33aqbc=>"33abs"
 //null=> null
 //undefined=>undefined
+
+// addd string
+// let str1="vikas"
+// let str2="yadav"
+// console.log(str1+str2)
+
+// code that we should not do
+
+// console.log("1"+2+2)
+// console.log(2+3+"4")
+// console.log(3+"5"+4)
+// console.log(3+2+3)
+
+console.log(+true)
+console.log(+"")
+
+// pre incresment and post increament
+
+let sum=5;
+let sum1=sum++
+let sum2=++sum
+console.table([sum,sum1,sum2])
