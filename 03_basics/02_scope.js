4df69e43e8a7b3a7921fcb03114e0c4bcc8b4a3d
vikasyadav104 <vikasyadavskb247@gmail.com>
2025-12-25 08:27:05 +0000

    chANGES

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,38 @@
+ //why we not use var instead of let
+
+ if(true){
+    let a=10
+    const b=20
+    // var c=200 never use it 
+ }
+//  console.log(a) //they will not print coz a is defined only in loop not outside from loop
+//  console.log(b) //they will not print coz a is defined only in loop not outside from loop
+//  console.log(c)   //but it will print this will give more confusion so we never use var in loop
+
+ //Nested Function
+
+ function one(){
+    const website= "vikas@google.com"
+    function two(){
+        const website2="monti@gmail.com"
+        console.log(website2)
+    }
+    //two()
+ }
+//  one() 
+
+if(true){
+        const username="vikas"
+        if(username==="vikas"){
+            const web=" youtube"
+            // console.log(username+web)
+        }
+}
+
+//as this much knowledge we know that fucntion can be defined in two way
+
+//1st one
+
+
+fu
+
