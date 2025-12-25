13f2b26b855ba8c27914cf37f2cdd5689d2d15b6
vikasyadav104 <vikasyadavskb247@gmail.com>
2025-12-25 05:44:54 +0000

    function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -44,6 +44,41 @@
     }
         return `${username} just logged in`
  }
- console.log(loginusername())
+ console.log(loginusername())  
 
- 
+ //Some important keys 
+
+//if we dont know number of values passes in function for examplw
+
+// function myCartNumber(num1){
+//     return num1
+
+// }
+// console.log(myCartNumber(200,300,400))
+//here er have three values but in function only one parameter so ho can we manage
+
+
+//we use three dots
+ function myCartNumber(...num1){
+     return num1
+ }
+ console.log(myCartNumber(200,400,500)) //this print all the value present as paramater
+
+ const user={
+    name:"vikas",
+    price:"3000",
+
+ }
+ function handleObject(anyobject){
+    console.log(`username id ${anyobject.name} and price is ${anyobject.price}`)
+ }
+
+//  handleObject(user)
+//we can also print data directly
+
+handleObject({
+    name:"sam",
+    price:"300",
+})
+
+ ///it will print username id vikas and price is 300
