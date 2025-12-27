714ea6eb08bed9866215834a5c7b5920b36d06e0
vikasyadav104 <vikasyadavskb247@gmail.com>
2025-12-27 17:22:04 +0000

    changes

---
+++ 05_iterations./one.js
@@ -0,0 +1,27 @@
+//For loop
+
+// for(let i=0; i<=10; i++){
+//     const element=i;
+//     console.log(element)
+// }
+
+
+//we can also do this in two loop also
+
+// for(let i=0;i<10; i++){
+//     console.log(`Outer Loop value: ${i}`);
+//     for(let j=0; j<=10; j++){
+//         console.log(`product of ${i} and ${j} is equal to ${i*j} `)
+//     }
+// }
+
+//lets work this loop in arryas
+
+let Array=["vikas","dibyam","aditya","aman"]
+console.log(Array.length)
+ for(let i=0; i<Array.length; i++){
+    console.log(Array[i])
+ }
+ //break and continue
+
+ //same as cpp
